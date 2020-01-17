2e719b71f4ead6642549a0e65dfcafda23ea7f35
Xavier Gillmann <xgillmann@bluesquarehub.com>
2020-01-17 10:53:17 +0100

    OMT-77 merge conflicts

--- claim/validations.py
+++ claim/validations.py
@@ -169,38 +169,22 @@ def validate_claimservice_validity(claimservice):
 
 
 def validate_claimitem_in_price_list(claim, claimitem):
-<<<<<<< HEAD
-    pricelist_detail = ItemPricelistDetail.objects\
-        .filter(item_pricelist=claim.health_facility.item_pricelist)\
-        .filter(item_id=claimitem.item_id)\
-        .filter(validity_to__isnull=True)\
-        .filter(item_pricelist__validity_to__isnull=True)\
-=======
     pricelist_detail = ItemsPricelistDetail.objects\
         .filter(items_pricelist=claim.health_facility.items_pricelist)\
         .filter(item_id=claimitem.item_id)\
         .filter(validity_to__isnull=True)\
         .filter(items_pricelist__validity_to__isnull=True)\
->>>>>>> feature/OMT-77
         .first()
     if not pricelist_detail:
         claimitem.rejection_reason = REJECTION_REASON_NOT_IN_PRICE_LIST
 
 
 def validate_claimservice_in_price_list(claim, claimservice):
-<<<<<<< HEAD
-    pricelist_detail = ServicePricelistDetail.objects\
-        .filter(service_pricelist=claim.health_facility.service_pricelist)\
-        .filter(service_id=claimservice.service_id)\
-        .filter(validity_to__isnull=True)\
-        .filter(service_pricelist__validity_to__isnull=True)\
-=======
     pricelist_detail = ServicesPricelistDetail.objects\
         .filter(services_pricelist=claim.health_facility.services_pricelist)\
         .filter(service_id=claimservice.service_id)\
         .filter(validity_to__isnull=True)\
         .filter(services_pricelist__validity_to__isnull=True)\
->>>>>>> feature/OMT-77
         .first()
     if not pricelist_detail:
         claimservice.rejection_reason = REJECTION_REASON_NOT_IN_PRICE_LIST
