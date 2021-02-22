4f7b73a712934681f7bdf5beffcc28effce2ccd0
Eric Darchis <darchis@pobox.com>
2021-02-22 06:51:50 +0100

    generate claim admins, add HF, code, admin to claim

---
+++ claim/management/commands/generateclaimadmins.py
@@ -0,0 +1,53 @@
+import random
+import string
+
+from claim.test_helpers import create_test_claim_admin
+from django.core.management.base import BaseCommand
+from faker import Faker
+
+
+class Command(BaseCommand):
+    help = "This command will generate test Claims Administrators. It is intended to simulate larger" \
+           "databases for performance testing. Some locations have almost 1000 of them."
+    insurees = None
+    services = None
+    items = None
+    claim_admins = None
+    hfs = None
+
+    def add_arguments(self, parser):
+        parser.add_argument("nb_admins", nargs=1, type=int)
+        parser.add_argument(
+            '--verbose',
+            action='store_true',
+            dest='verbose',
+            help='Be verbose about what it is doing',
+        )
+        parser.add_argument(
+            '--locale',
+            default="fr_FR",
+            help="Used to adapt the fake names generation to the locale, using Faker, by default fr_FR",
+        )
+
+    def handle(self, *args, **options):
+
+        nb_admins = options["nb_admins"][0]
+        verbose = options["verbose"]
+        fake = Faker(options["locale"])
+        for admin_num in range(1, nb_admins + 1):
+            hf = self.get_random_hf()
+            claim_admin = create_test_claim_admin({
+                "code": ''.join(random.choices(string.ascii_uppercase + string.digits, k=8)),
+                "last_name": fake.last_name(),
+                "other_names": fake.first_name(),
+                "email_id": fake.ascii_email(),
+                "phone": "+" + fake.msisdn(),
+                "health_facility_id": hf})
+            if verbose:
+                print(admin_num, "created claim admin", claim_admin, "for HF", hf, "with code", claim_admin.code)
+
+    def get_random_hf(self):
+        if not self.hfs:
+            from location.models import HealthFacility
+            self.hfs = HealthFacility.objects.filter(validity_to__isnull=True).values_list("pk", flat=True)
+        return random.choice(self.hfs)
--- claim/management/commands/generateclaims.py
+++ claim/management/commands/generateclaims.py
@@ -1,5 +1,7 @@
 import random
+import string
 
+from claim.models import ClaimAdmin
 from claim.test_helpers import create_test_claim, create_test_claimservice, create_test_claimitem
 from django.core.management.base import BaseCommand
 from insuree.models import Insuree
@@ -12,6 +14,8 @@ class Command(BaseCommand):
     insurees = None
     services = None
     items = None
+    claim_admins = None
+    hfs = None
 
     def add_arguments(self, parser):
         parser.add_argument("nb_claims", nargs=1, type=int)
@@ -31,9 +35,14 @@ class Command(BaseCommand):
         verbose = options["verbose"]
         for claim_num in range(1, nb_claims + 1):
             insuree = self.get_random_insuree()
-            claim = create_test_claim({"insuree_id": insuree})
+            claim_admin = self.get_random_claim_admin()
+            hf = self.get_random_hf()
+            claim = create_test_claim({"insuree_id": insuree,
+                                       "code": ''.join(random.choices(string.ascii_uppercase + string.digits, k=8)),
+                                       "admin_id": claim_admin,
+                                       "health_facility_id": hf})
             if verbose:
-                print(claim_num, "created claim", claim, "for insuree", insuree)
+                print(claim_num, "created claim", claim, "for insuree", insuree, "with code", claim.code)
             for svc_num in range(1, nb_services + 1):
                 service = self.get_random_service()
                 claim_service = create_test_claimservice(claim, custom_props={"service_id": service})
@@ -54,3 +63,14 @@ class Command(BaseCommand):
         if not self.services:
             self.services = Service.objects.filter(validity_to__isnull=True).values_list("pk", flat=True)
         return random.choice(self.services)
+
+    def get_random_claim_admin(self):
+        if not self.claim_admins:
+            self.claim_admins = ClaimAdmin.objects.filter(validity_to__isnull=True).values_list("pk", flat=True)
+        return random.choice(self.claim_admins)
+
+    def get_random_hf(self):
+        if not self.hfs:
+            from location.models import HealthFacility
+            self.hfs = HealthFacility.objects.filter(validity_to__isnull=True).values_list("pk", flat=True)
+        return random.choice(self.hfs)
