57dfb6608b59d4611c729b132e5a7aa85a89cce2
Thibault Dethier <84660492+tdethier@users.noreply.github.com>
2023-08-31 09:00:52 +0200

    OMT-314 - Claim History report (#129)
    * WIP - Added claims overview report v0.5
    
    * OMT-321 - Claims Overview report
    
    * OMT-314 - Claim History report

--- claim/report.py
+++ claim/report.py
@@ -1,5 +1,7 @@
-from claim.reports import claim_percentage_referrals
+from claim.reports import claim_percentage_referrals, claims_overview, claim_history
+from claim.reports.claim_history import claim_history_query
 from claim.reports.claim_percentage_referrals import claim_percentage_referrals_query
+from claim.reports.claims_overview import claims_overview_query
 
 report_definitions = [
     {
@@ -10,5 +12,23 @@ report_definitions = [
         "module": "claim",
         "python_query": claim_percentage_referrals_query,
         "permission": ["131214"],
+    },
+    {
+        "name": "claims_overview",
+        "engine": 0,
+        "default_report": claims_overview.template,
+        "description": "Overview of the processing of claims",
+        "module": "claim",
+        "python_query": claims_overview_query,
+        "permission": ["131213"],
+    },
+    {
+        "name": "claim_history",
+        "engine": 0,
+        "default_report": claim_history.template,
+        "description": "Claim history",
+        "module": "claim",
+        "python_query": claim_history_query,
+        "permission": ["131223"],
     },
 ]
