3580f87b51274d4651ef4addd2c4d4023fe25de0
Hiren Soni <hirensoni913@gmail.com>
2023-12-08 16:10:05 +0100

    Support added for MSSQL

--- claim/reports/claim_percentage_referrals.py
+++ claim/reports/claim_percentage_referrals.py
@@ -395,9 +395,9 @@ template = """
                             "elementType": "table_text",
                             "id": 277,
                             "width": 304,
-                            "content": "${hf}",
+                            "content": "${HF}",
                             "eval": false,
-                            "colspan": "",
+                            "colspan": "",  
                             "styleId": "",
                             "bold": false,
                             "italic": false,
@@ -441,7 +441,7 @@ template = """
                             "elementType": "table_text",
                             "id": 278,
                             "width": 90,
-                            "content": "${totalclaims}",
+                            "content": "${TotalClaims}",
                             "eval": false,
                             "colspan": "",
                             "styleId": "",
@@ -487,7 +487,7 @@ template = """
                             "elementType": "table_text",
                             "id": 283,
                             "width": 90,
-                            "content": "${totalop}",
+                            "content": "${TotalOP}",
                             "eval": false,
                             "colspan": "",
                             "styleId": "",
@@ -533,7 +533,7 @@ template = """
                             "elementType": "table_text",
                             "id": 286,
                             "width": 90,
-                            "content": "${totalip}",
+                        "content": "${TotalIP}",
                             "eval": false,
                             "colspan": "",
                             "styleId": "",
@@ -977,7 +977,7 @@ template = """
                 },
                 {
                     "id": 235,
-                    "name": "hf",
+                    "name": "HF",
                     "type": "string",
                     "arrayItemType": "string",
                     "eval": false,
@@ -989,7 +989,7 @@ template = """
                 },
                 {
                     "id": 236,
-                    "name": "totalclaims",
+                    "name": "TotalClaims",
                     "type": "number",
                     "arrayItemType": "string",
                     "eval": false,
@@ -1001,7 +1001,7 @@ template = """
                 },
                 {
                     "id": 262,
-                    "name": "totalop",
+                    "name": "TotalOP",
                     "type": "number",
                     "arrayItemType": "string",
                     "eval": false,
@@ -1013,7 +1013,7 @@ template = """
                 },
                 {
                     "id": 271,
-                    "name": "totalip",
+                    "name": "TotalIP",
                     "type": "number",
                     "arrayItemType": "string",
                     "eval": false,
@@ -1131,67 +1131,120 @@ template = """
 """
 
 # TODO transform the SQL query into a Django ORM query
-percentage_referrals_sql = """
-SELECT CONCAT(HF."HFCode", ' - ', HF."HFName") HF, TotalClaim.TotalClaims, RefOP.TotalOP, RefIP.TotalIP
-FROM (SELECT HF."HfID", HF."HFCode", HF."HFName"
-      FROM "tblHF" HF
-               INNER JOIN "uvwLocations" L ON L."LocationId" = HF."LocationId"
-      WHERE HF."ValidityTo" Is NULL
-        AND HF."HFLevel" IN ('D', 'C')
-        AND (L."RegionId" = %(region_id)s OR %(region_id)s = 0 OR L."LocationId" IS NULL)
-        AND (L."DistrictId" = %(district_id)s OR %(district_id)s = 0 OR L."DistrictId" IS NULL)
+if connection.vendor == "postgresql":
+    percentage_referrals_sql = """
+    SELECT CONCAT(HF."HFCode", ' - ', HF."HFName") HF, TotalClaim.TotalClaims, RefOP.TotalOP, RefIP.TotalIP
+    FROM (SELECT HF."HfID", HF."HFCode", HF."HFName"
+        FROM "tblHF" HF
+                INNER JOIN "uvwLocations" L ON L."LocationId" = HF."LocationId"
+        WHERE HF."ValidityTo" Is NULL
+            AND HF."HFLevel" IN ('D', 'C')
+            AND (L."RegionId" = %(region_id)s OR %(region_id)s = 0 OR L."LocationId" IS NULL)
+            AND (L."DistrictId" = %(district_id)s OR %(district_id)s = 0 OR L."DistrictId" IS NULL)
+        ) HF
+            LEFT OUTER JOIN (SELECT COUNT(1) TotalClaims, "HFID"
+                            FROM "tblClaim"
+                            WHERE "ValidityTo" Is NULL
+                            AND "DateClaimed" BETWEEN %(date_start)s AND %(date_end)s
+                            GROUP BY "HFID") TotalClaim ON HF."HfID" = TotalClaim."HFID"
+            LEFT OUTER JOIN (SELECT I."HFID", COUNT(C."ClaimID") TotalOP
+                            FROM "tblClaim" C
+                                    INNER JOIN "tblInsuree" I ON C."InsureeID" = I."InsureeID"
+                                    INNER JOIN "tblHF" HF ON C."HFID" = HF."HfID"
+                                    INNER JOIN "uvwLocations" L ON L."LocationId" = HF."LocationId"
+                            WHERE C."ValidityTo" Is NULL
+                                AND I."ValidityTo" IS NULL
+                                AND HF."ValidityTo" IS NULL
+                                AND (C."DateTo" is null OR C."DateFrom" = C."DateTo")
+                                AND HF."HfID" <> I."HFID"
+                                AND C."VisitType" = N'R'
+                                AND (L."RegionId" = %(region_id)s OR %(region_id)s = 0 OR L."LocationId" IS NULL)
+                                AND (L."DistrictId" = %(district_id)s OR %(district_id)s = 0 OR L."DistrictId" IS NULL)
+                                AND C."DateClaimed" BETWEEN %(date_start)s AND %(date_end)s
+                            GROUP BY I."HFID") RefOP ON HF."HfID" = RefOP."HFID"
+            LEFT OUTER JOIN (SELECT I."HFID", COUNT(C."ClaimID") TotalIP
+                            FROM "tblClaim" C
+                                    INNER JOIN "tblInsuree" I ON C."InsureeID" = I."InsureeID"
+                                    INNER JOIN "tblHF" HF ON C."HFID" = HF."HfID"
+                                    INNER JOIN "uvwLocations" L ON L."LocationId" = HF."LocationId"
+                            WHERE C."ValidityTo" Is NULL
+                                AND I."ValidityTo" IS NULL
+                                AND HF."ValidityTo" IS NULL
+                                AND C."DateTo" is not null and C."DateFrom" <> C."DateTo"
+                                AND HF."HfID" <> I."HFID"
+                                AND C."VisitType" = N'R'
+                                AND (L."RegionId" = %(region_id)s OR %(region_id)s = 0 OR L."LocationId" IS NULL)
+                                AND (L."DistrictId" = %(district_id)s OR %(district_id)s = 0 OR L."DistrictId" IS NULL)
+                                AND C."DateClaimed" BETWEEN %(date_start)s AND %(date_end)s
+                            GROUP BY I."HFID") RefIP ON HF."HfID" = RefIP."HFID"
+    """
+else:
+    percentage_referrals_sql = """
+    SELECT CONCAT(HF.HFCode, ' - ', HF.HFName) HF, TotalClaim.TotalClaims, RefOP.TotalOP, RefIP.TotalIP
+    FROM (SELECT HF.HfID, HF.HFCode, HF.HFName
+      FROM tblHF HF
+               INNER JOIN uvwLocations L ON L.LocationId = HF.LocationId
+      WHERE HF.ValidityTo Is NULL
+        AND HF.HFLevel IN ('D', 'C')
+        AND (L.RegionId = %s OR %s = 0 OR L.LocationId IS NULL)
+        AND (L.DistrictId = %s OR %s = 0 OR L.DistrictId IS NULL)
       ) HF
-         LEFT OUTER JOIN (SELECT COUNT(1) TotalClaims, "HFID"
-                          FROM "tblClaim"
-                          WHERE "ValidityTo" Is NULL
-                          AND "DateClaimed" BETWEEN %(date_start)s AND %(date_end)s
-                          GROUP BY "HFID") TotalClaim ON HF."HfID" = TotalClaim."HFID"
-         LEFT OUTER JOIN (SELECT I."HFID", COUNT(C."ClaimID") TotalOP
-                          FROM "tblClaim" C
-                                   INNER JOIN "tblInsuree" I ON C."InsureeID" = I."InsureeID"
-                                   INNER JOIN "tblHF" HF ON C."HFID" = HF."HfID"
-                                   INNER JOIN "uvwLocations" L ON L."LocationId" = HF."LocationId"
-                          WHERE C."ValidityTo" Is NULL
-                            AND I."ValidityTo" IS NULL
-                            AND HF."ValidityTo" IS NULL
-                            AND (C."DateTo" is null OR C."DateFrom" = C."DateTo")
-                            AND HF."HfID" <> I."HFID"
-                            AND C."VisitType" = N'R'
-                            AND (L."RegionId" = %(region_id)s OR %(region_id)s = 0 OR L."LocationId" IS NULL)
-                            AND (L."DistrictId" = %(district_id)s OR %(district_id)s = 0 OR L."DistrictId" IS NULL)
-                            AND C."DateClaimed" BETWEEN %(date_start)s AND %(date_end)s
-                          GROUP BY I."HFID") RefOP ON HF."HfID" = RefOP."HFID"
-         LEFT OUTER JOIN (SELECT I."HFID", COUNT(C."ClaimID") TotalIP
-                          FROM "tblClaim" C
-                                   INNER JOIN "tblInsuree" I ON C."InsureeID" = I."InsureeID"
-                                   INNER JOIN "tblHF" HF ON C."HFID" = HF."HfID"
-                                   INNER JOIN "uvwLocations" L ON L."LocationId" = HF."LocationId"
-                          WHERE C."ValidityTo" Is NULL
-                            AND I."ValidityTo" IS NULL
-                            AND HF."ValidityTo" IS NULL
-                            AND C."DateTo" is not null and C."DateFrom" <> C."DateTo"
-                            AND HF."HfID" <> I."HFID"
-                            AND C."VisitType" = N'R'
-                            AND (L."RegionId" = %(region_id)s OR %(region_id)s = 0 OR L."LocationId" IS NULL)
-                            AND (L."DistrictId" = %(district_id)s OR %(district_id)s = 0 OR L."DistrictId" IS NULL)
-                            AND C."DateClaimed" BETWEEN %(date_start)s AND %(date_end)s
-                          GROUP BY I."HFID") RefIP ON HF."HfID" = RefIP."HFID"
-"""
+         LEFT OUTER JOIN (SELECT COUNT(1) TotalClaims, HFID
+                          FROM tblClaim
+                          WHERE ValidityTo Is NULL
+                          AND DateClaimed BETWEEN %s AND %s
+                          GROUP BY HFID) TotalClaim ON HF.HfID = TotalClaim.HFID
+         LEFT OUTER JOIN (SELECT I.HFID, COUNT(C.ClaimID) TotalOP
+                          FROM tblClaim C
+                                   INNER JOIN tblInsuree I ON C.InsureeID = I.InsureeID
+                                   INNER JOIN tblHF HF ON C.HFID = HF.HfID
+                                   INNER JOIN uvwLocations L ON L.LocationId = HF.LocationId
+                          WHERE C.ValidityTo Is NULL
+                            AND I.ValidityTo IS NULL
+                            AND HF.ValidityTo IS NULL
+                            AND (C.DateTo is null OR C.DateFrom = C.DateTo)
+                            AND HF.HfID <> I.HFID
+                            AND C.VisitType = N'R'
+                            AND (L.RegionId = %s OR %s = 0 OR L.LocationId IS NULL)
+                            AND (L.DistrictId = %s OR %s = 0 OR L.DistrictId IS NULL)
+                            AND C.DateClaimed BETWEEN %s AND %s
+                          GROUP BY I.HFID) RefOP ON HF.HfID = RefOP.HFID
+         LEFT OUTER JOIN (SELECT I.HFID, COUNT(C.ClaimID) TotalIP
+                          FROM tblClaim C
+                                   INNER JOIN tblInsuree I ON C.InsureeID = I.InsureeID
+                                   INNER JOIN tblHF HF ON C.HFID = HF.HfID
+                                   INNER JOIN uvwLocations L ON L.LocationId = HF.LocationId
+                          WHERE C.ValidityTo Is NULL
+                            AND I.ValidityTo IS NULL
+                            AND HF.ValidityTo IS NULL
+                            AND C.DateTo is not null and C.DateFrom <> C.DateTo
+                            AND HF.HfID <> I.HFID
+                            AND C.VisitType = N'R'
+                            AND (L.RegionId = %s OR %s = 0 OR L.LocationId IS NULL)
+                            AND (L.DistrictId = %s OR %s = 0 OR L.DistrictId IS NULL)
+                            AND C.DateClaimed BETWEEN %s AND %s
+                          GROUP BY I.HFID) RefIP ON HF.HfID = RefIP.HFID
+    """
 
 
 def claim_percentage_referrals_query(user, region_id=0, district_id=0, date_start="2019-01-01", date_end="2022-12-31",
                                      **kwargs):
     with connection.cursor() as cur:
         try:
-            cur.execute(
-                percentage_referrals_sql,
-                {
-                    "region_id": region_id,
-                    "district_id": district_id,
-                    "date_start": date_start,
-                    "date_end": date_end,
-                },
-            )
+            if connection.vendor == "postgresql":
+                cur.execute(
+                    percentage_referrals_sql,
+                    {
+                        "region_id": region_id,
+                        "district_id": district_id,
+                        "date_start": date_start,
+                        "date_end": date_end,
+                    },
+                )
+            else:
+                cur.execute(percentage_referrals_sql, (region_id, region_id, district_id, district_id, date_start, date_end, region_id, region_id,
+                            district_id, district_id, date_start, date_end, region_id, region_id, district_id, district_id, date_start, date_end))
+
             data = dictfetchall(cur)
             return {"data": data}
         except Exception as e:
