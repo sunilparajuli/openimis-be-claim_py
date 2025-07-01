ed4449f5905edf334531ff1d5afe9da3f71382b2
Patrick Delcroix <delcroip@gmail.com>
2025-07-01 09:07:42 +0200

    Update claim/validations.py

--- claim/validations.py
+++ claim/validations.py
@@ -223,8 +223,7 @@ def validate_claimitem_in_price_list(claim, claimitem):
     target_date = get_claim_target_date(claim)
     pricelist_detail_qs = ItemsPricelistDetail.objects \
         .filter(item_id=claimitem.item_id,
-                validity_to__gte=target_date,
-                validity_from__lte=target_date,
+                 *filter_validity(validity=target_date)
                 items_pricelist=claim.health_facility.items_pricelist,
                 items_pricelist__validity_to__isnull=True
                 )
